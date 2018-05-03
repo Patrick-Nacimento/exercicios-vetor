55d34e8e830a44155036e22c464e3ec3baaa5657
Patrick-Nacimento <38558599+Patrick-Nacimento@users.noreply.github.com>
2018-05-03 11:25:43 -0300

    iniciando exercicios vetor

--- ExerVetor1.java
+++ ExerVetor1.java
@@ -2,6 +2,28 @@ import javax.swing.JOptionPane;
 
 public class ExerVetor1{
     public static void main(String[] args){
+
+        int[] numeros = new int[16];
+        numeros[0] = 7;
+        numeros[1] = 17;
+        numeros[2] = 27;
+        numeros[3] = 37;
+        numeros[4] = 47;
+        numeros[5] = 57;
+        numeros[6] = 67;
+        numeros[7] = 70;
+        numeros[8] = 71;
+        numeros[9] = 72;
+        numeros[10] = 73;
+        numeros[11] = 74;
+        numeros[12] = 75;
+        numeros[13] = 76;
+        numeros[14] = 78;
+        numeros[15] = 79;
+        int soma = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6] + numeros[7] + numeros[8] + numeros[9] + numeros[10] + numeros[11] + numeros[12] + numeros[13] + numeros[14] + numeros[15] + numeros[15];
+
+        System.out.print("Soma: " + soma);
+
         
     }
 }
---
+++ ExerVetor2.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class ExerVetor2{
+    public static void main(String[] args){
+
+        String[] nomes = new String[10];
+        nomes[0] = 
+
+    }
+}
